13bc5ca5ca0399f1ec7e063b5399afbae1f40cd5
admiralpxl <wnstnjr@gmail.com>
2021-09-23 14:32:57 -0400

    links for icons, icon page, title, color and description added in the head

--- components/Logo.js
+++ components/Logo.js
@@ -1,11 +1,11 @@
 import React from "react";
-import svg from "./assets/isotipo-vector.svg";
-//  
+
 function Logo(){
+    const url = "https://lh3.googleusercontent.com/b7VtxgzXBgMDn4OUthllpyhvhPuBdAPOWqzz6eHIAZmrqhiFFmKlM_SFLGkT9y6cFHJvSrabDKYRlWuHPeQRb-wU9TQFUpMvd4sGRuW1yK4Oc02yfbPMmFVCUrrZUNcqjiCKBfessadQJIPiN3ZVpgAZaPxpI2VibhRURxyYeLT2x02fsbeD2QBtjkiABOgGi6sfNCl7_vsauGVkHralBhGtCvcu_QM0Z22i1rS-Dure8aDa7Pjrp380xvJC-oV-K0-MwHL6hH9jdFNhMZ5ZfMd9-lsj7QY9gGFyZDvFqtahV6wqBPiwFfxp4BJoNutMIJmSM23R9nPS-RISGCpAi_2j2EezVqEIg8HIUGc5k_l0Xe9aUw9GICS_CTF_FkStruXDKYasGQGFyEc5EC1ZsY8idlBVG47gu0Nw3uncNlRb17DYlDlyRMQ0BcsWE6gBYXTZ-Eym1FD1ZnhiZZNEdCf8ktDveJhskKhDGzCoIQkXQC-Imb5qXDKcVz1IttzvI31NGHTuESjwUZbZjDPVZdNIUcwvIvGEoRlfRbxlRnjde5elIc1U7Iw1ry-Cp5wBS548jSFjOnWtMjFiX7jfBW3VlvL1iJVAbSYMCDc5Hp435_x8uPbR7RJG1XW-yYuW52gDirHiB4q_i-fCHoxWlJcSRj8ztFOFpANdJBcHQkI4kFyywk0v1vbQbXo-Oq_6bGbHO7lvfYxvWPYNqssI9HQ=w225-h50-no?authuser=1";
     return(
         <React.Fragment>
            <figure>
-            <img src="./assets/isotipo-vector.svg" alt="Logotipo"/>
+            <img src={ url } alt="Logotipo"/>
             </figure>
             <h3>ADMIRALPXL</h3>
             <style jsx>{`
