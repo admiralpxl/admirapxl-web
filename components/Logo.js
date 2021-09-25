dae3a7cc2ea3f02cc17f2bfd08fe6b96825f2d56
admiralpxl <wnstnjr@gmail.com>
2021-09-25 12:51:59 -0400

    Logo component finished

--- components/Logo.js
+++ components/Logo.js
@@ -1,20 +1,39 @@
 import React from "react";
 
-function Logo(){
-    return(
-        <React.Fragment>
-           <figure>
-            <img src="isotipo-vector.svg" alt="Logotipo"/>
-            </figure>
-            <h3>ADMIRALPXL</h3>
-            <style jsx>{`
-                figure {
-                    width: 200px;
-                    height: 200px;
-                }
-            `}</style>
-        </React.Fragment>
-    )
+function Logo() {
+  return (
+    <article className="flex-column center">
+      <figure>
+        <img src="isotipo-vector.svg" alt="Logotipo" />
+      </figure>
+      <h3>ADMIRALPXL</h3>
+      <style jsx>{`
+        section {
+          width: 100%;
+          height: auto;
+        }
+        figure {
+          width: 100px;
+          height: 100px;
+          margin-bottom: 12px;
+        }
+        h3 {
+          font-family: var(--merri);
+          font-size: var(--logo-font);
+          font-weight: var(--bold);
+        }
+        @media (min-width: 768px) {
+          figure {
+            width: 150px;
+            height: 150px;
+          }
+          h3 {
+            font-size: var(--menu);
+          }
+        }
+      `}</style>
+    </article>
+  );
 }
 
 export { Logo };
