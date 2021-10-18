2c9515d52008b0347b31b619e401c0e8f308cc3d
admiralpxl <wnstnjr@gmail.com>
2021-10-18 12:40:09 -0500

    portfolio css fixed

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -48,7 +48,7 @@ function Portfolio() {
       <Paragraph content="These projects help me to learn and improve more my frontend developer skills." />
       <section className="portfolio-projects">
         <div className="grid-one gridded">
-          <ProjectsTwo
+          <Projects
             image="https://d33wubrfki0l68.cloudfront.net/616cbed5fe4d44e8064753aa/screenshot_2021-10-18-00-26-01-0000.png"
             title="JuniorHub"
             content="This App shows you the user information and their repositories, consulting the GitHub API, using async functions to get a more synchronous code, and the React Hooks useState to handle the information and states."
@@ -58,7 +58,7 @@ function Portfolio() {
           />
         </div>
         <div className="grid-two gridded">
-          <Projects
+          <ProjectsTwo
             image="https://d33wubrfki0l68.cloudfront.net/6101f5088f644325dbbcb25a/screenshot_2021-07-29-00-23-41-0000.png"
             title="The Lion´s Song"
             content="This is a redesign of the landing page of The Lion´s Song Game by Mi'pu'mi Games, I made the design on Figma, practice my knowledge of transform, transition, and animation property of CSS also uses Vanilla Javascript for made more dynamic the page, the design it is optimized for mobile, tablet and Desktop."
@@ -68,7 +68,7 @@ function Portfolio() {
           />
         </div>
         <div className="grid-three gridded">
-          <ProjectsTwo
+          <Projects
             image="https://d33wubrfki0l68.cloudfront.net/611194b1dce5780008b8329e/screenshot_2021-08-09-20-48-53-0000.png"
             title="Forescast Today App"
             content="A beautiful and minimalist weather app, made with pure HTML, CSS, and Vanilla Javascript, the app use OpenWeather API to get the information and show it through DOM manipulation and using AJAX template."
@@ -78,7 +78,7 @@ function Portfolio() {
           />
         </div>
         <div className="grid-four gridded">
-          <Projects
+          <ProjectsTwo
             image="https://d33wubrfki0l68.cloudfront.net/60a17f064fde3d000776fcf7/screenshot_2021-05-16-20-22-35-0000.png"
             title="INTERACTIVE PRICE SELECTOR"
             content="This is a interactive price component make with SASS and Javascript, I also practice array and manipule DOM elements like the input range and check boxs."
